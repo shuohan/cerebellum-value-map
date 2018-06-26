8c023236509b3babbb61552834a0eed6ddfe673b
Shuo Han <shan50@jhu.edu>
2018-06-26 17:59:57 -0400

    Change class order in polygon.py

--- cerebellum_value_map/polygon.py
+++ cerebellum_value_map/polygon.py
@@ -20,6 +20,39 @@ import numpy as np
 pattern_type = 'stripe'
 
 
+class ColorConverter:
+
+    def __init__(self, min_color_value=-1, max_color_value=1, colormap='RdBu_r',
+                 max_disabling_value=0.05, pattern_name='stripe'):
+        self.max_disabling_value = max_disabling_value
+        self.pattern_name = pattern_name
+        norm = Normalize(vmin=min_color_value, vmax=max_color_value)
+        self.colormap = ScalarMappable(norm, cmap=colormap)
+
+    def convert(self, color_value, disabling_value=-float('inf')):
+        if  disabling_value <= self.max_disabling_value:
+            color = self.colormap.to_rgba(color_value, bytes=True)[:3]
+            result = 'rgb(%s)' % ', '.join([str(c) for c in color])
+        else:
+            result = "url(#%s)" % (self.pattern_name)
+        return result
+
+
+class Stripe(Defs):
+    def __init__(self):
+        # from https://philiprogers.com/svgpatterns/#thinstripes
+        super().__init__()
+        rect = Rect(insert=(0, 0), size=(5, 5), fill='#9e9e9e')
+        path = Path('M 0 5 L 5 0 Z M 6 4 L 4 6 Z M -1 1 L 1 -1 Z',
+                    stroke='#888', stroke_width=1)
+        pattern = Pattern(id=self.__class__.__name__.lower(),
+                          patternUnits='userSpaceOnUse',
+                          size=(5, 5), stroke='none')
+        pattern.add(rect)
+        pattern.add(path)
+        self.add(pattern)
+
+
 class PolygonBoundingBox:
     def __init__(self, polygon_points):
         polygon_points = np.array(polygon_points)
@@ -98,39 +131,6 @@ class AnotatedPolygon(Group):
         self.add(Polygon(points, fill=color))
         self.add(Value(value['color'], anchor_bbox))
         self.add(Anotation(anot['text'], anot['position'], anchor_bbox))
-
-
-class ColorConverter:
-
-    def __init__(self, min_color_value=-1, max_color_value=1, colormap='RdBu_r',
-                 max_disabling_value=0.05, pattern_name='stripe'):
-        self.max_disabling_value = max_disabling_value
-        self.pattern_name = pattern_name
-        norm = Normalize(vmin=min_color_value, vmax=max_color_value)
-        self.colormap = ScalarMappable(norm, cmap=colormap)
-
-    def convert(self, color_value, disabling_value=-float('inf')):
-        if  disabling_value <= self.max_disabling_value:
-            color = self.colormap.to_rgba(color_value, bytes=True)[:3]
-            result = 'rgb(%s)' % ', '.join([str(c) for c in color])
-        else:
-            result = "url(#%s)" % (self.pattern_name)
-        return result
-
-
-class Stripe(Defs):
-    def __init__(self):
-        # from https://philiprogers.com/svgpatterns/#thinstripes
-        super().__init__()
-        rect = Rect(insert=(0, 0), size=(5, 5), fill='#9e9e9e')
-        path = Path('M 0 5 L 5 0 Z M 6 4 L 4 6 Z M -1 1 L 1 -1 Z',
-                    stroke='#888', stroke_width=1)
-        pattern = Pattern(id=self.__class__.__name__.lower(),
-                          patternUnits='userSpaceOnUse',
-                          size=(5, 5), stroke='none')
-        pattern.add(rect)
-        pattern.add(path)
-        self.add(pattern)
 
 
 
