ff23c53aa7877ed3c9c1023597f801b17f758419
Shuo Han <shan50@jhu.edu>
2018-07-03 18:05:27 -0400

    Add cerebellum value map

---
+++ cerebellum_value_map/cerebellum.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from svgwrite import Drawing
+
+from .lobes import RightAnteriorLobe, RightSuperiorPosteriorLobe
+from .lobes import RightInferiorPosteriorLobe, Vermis
+from .lobes import LeftAnteriorLobe, LeftSuperiorPosteriorLobe
+from .lobes import LeftInferiorPosteriorLobe
+from .lobules import RightLobuleX, LeftLobuleX
+from .color import Stripe
+
+
+class CerebellumValueMap:
+
+    regions = {'right_anterior_lobe': RightAnteriorLobe,
+               'left_anterior_lobe': LeftAnteriorLobe,
+               'right_superioer_posterior_lobe': RightSuperiorPosteriorLobe,
+               'left_superior_posterior_lobe': LeftSuperiorPosteriorLobe,
+               'right_inferior_posterior_lobe': RightInferiorPosteriorLobe,
+               'left_inferior_posterior_lobe': LeftInferiorPosteriorLobe,
+               'right_lobule_x': RightLobuleX,
+               'left_lobule_x': LeftLobuleX,
+               'vermis': Vermis}
+
+    def __init__(self, data, output_filename, font_size=12, stroke='black',
+                 stroke_width=2, size=(600, 400)):
+        drawing = Drawing(output_filename, size=[str(num) for num in size],
+                          stroke=stroke, stroke_width=stroke_width,
+                          font_size=font_size)
+        for index, values in data.iterrows():
+            region = self.regions[index](coloring_value=values['color'],
+                                         disabling_value=values['disable'])
+            drawing.add(region.get_svg())
+        drawing.save(pretty=True)
---
+++ tests/value_map.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+sys.path.insert(0, '..')
+import pandas as pd
+
+from cerebellum_value_map.cerebellum import CerebellumValueMap
+
+data = {'color':[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
+        'disable':[0, 0, 0, 0, 0, 0, 0, 0, 0]}
+index = ['right_anterior_lobe', 'left_anterior_lobe',
+         'right_superioer_posterior_lobe', 'left_superior_posterior_lobe',
+         'right_inferior_posterior_lobe', 'left_inferior_posterior_lobe',
+         'right_lobule_x', 'left_lobule_x', 'vermis']
+df = pd.DataFrame(data, index=index)
+
+CerebellumValueMap(df, 'test.svg')
