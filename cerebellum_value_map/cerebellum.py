ca48abbeaf2460b957bf303c3588f65140828aab
Shuo Han <shan50@jhu.edu>
2018-07-04 01:11:44 -0400

    Add corpus medullare

--- cerebellum_value_map/cerebellum.py
+++ cerebellum_value_map/cerebellum.py
@@ -6,7 +6,7 @@ from .lobes import RightAnteriorLobe, RightSuperiorPosteriorLobe
 from .lobes import RightInferiorPosteriorLobe, Vermis
 from .lobes import LeftAnteriorLobe, LeftSuperiorPosteriorLobe
 from .lobes import LeftInferiorPosteriorLobe
-from .lobules import RightLobuleX, LeftLobuleX
+from .lobules import RightLobuleX, LeftLobuleX, CorpusMedullare
 from .color import Stripe
 
 
@@ -20,10 +20,11 @@ class CerebellumValueMap:
                'left_inferior_posterior_lobe': LeftInferiorPosteriorLobe,
                'right_lobule_x': RightLobuleX,
                'left_lobule_x': LeftLobuleX,
-               'vermis': Vermis}
+               'vermis': Vermis,
+               'corpus_medullare': CorpusMedullare}
 
     def __init__(self, data, output_filename, show_color=False,
-                 font_size=12, stroke='black', stroke_width=2, size=(600, 400)):
+                 font_size=12, stroke='black', stroke_width=2, size=(550, 450)):
         drawing = Drawing(output_filename, size=[str(num) for num in size],
                           stroke=stroke, stroke_width=stroke_width,
                           font_size=font_size)
