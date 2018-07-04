b223f55c381a2c4ac11f8ff9fd3dd422593ac43f
Shuo Han <shan50@jhu.edu>
2018-07-04 01:16:44 -0400

    Add script to show cerebellum value map

---
+++ scripts/show_cerebellum.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Show cerebellum value map')
+parser.add_argument('-i', '--input-data')
+parser.add_argument('-o', '--output-svg')
+parser.add_argument('-p', '--is-pvalue-mode', default=False,
+                    action='store_true')
+
+args = parser.parse_args()
+
+from cerebellum_value_map.cerebellum import CerebellumValueMap
+import sys
+import pandas as pd
+import numpy as np
+from cerebellum_value_map.color import ColorConverter
+
+
+def convert_pval(pval, sign):
+    if pval > 0.05:
+        result = 0
+    elif pval > 0.01:
+        result = 1
+    elif pval > 0.001:
+        result = 2
+    else:
+        result = 3
+    result *= np.sign(sign)
+    return result
+    
+data = pd.read_csv(args.input_data, index_col=0)
+if args.is_pvalue_mode:
+    cc = ColorConverter(min_color_value=-4, max_color_value=4)
+    for index, row in data.iterrows():
+        data.at[index, 'color'] = convert_pval(data.at[index, 'color'],
+                                               data.at[index, 'sign'])
+
+cerebellum = CerebellumValueMap(data, args.output_svg, stroke_width=1)
