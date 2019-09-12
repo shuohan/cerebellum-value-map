dc9fc27e09ddaf1323c16d145596570878c1cabf
Shuo Han <shan50@jhu.edu>
2019-09-11 20:48:06 -0400

    Add cerebellum lobule map

--- cerebellum_value_map/cerebellum.py
+++ cerebellum_value_map/cerebellum.py
@@ -6,22 +6,30 @@ from .lobes import RightAnteriorLobe, RightSuperiorPosteriorLobe
 from .lobes import RightInferiorPosteriorLobe, Vermis
 from .lobes import LeftAnteriorLobe, LeftSuperiorPosteriorLobe
 from .lobes import LeftInferiorPosteriorLobe
-from .lobules import RightLobuleX, LeftLobuleX, CorpusMedullare
-from .color import Stripe
+from .lobules import CorpusMedullare
+from .lobules import RightLobuleI2III, LeftLobuleI2III
+from .lobules import RightLobuleIV, LeftLobuleIV
+from .lobules import RightLobuleV, LeftLobuleV
+from .lobules import RightLobuleVI, LeftLobuleVI
+from .lobules import RightLobuleCrusI, LeftLobuleCrusI
+from .lobules import RightLobuleCrusII, LeftLobuleCrusII
+from .lobules import RightLobuleVIIB, LeftLobuleVIIB
+from .lobules import RightLobuleVIIIA, LeftLobuleVIIIA
+from .lobules import RightLobuleVIIIB, LeftLobuleVIIIB
+from .lobules import RightLobuleIX, LeftLobuleIX
+from .lobules import RightLobuleX, LeftLobuleX
+from .lobules import VermisVI
+from .lobules import VermisVII
+from .lobules import VermisVIII
+from .lobules import VermisIX
+from .lobules import VermisX
+
+# from .color import Stripe
 
 
 class CerebellumValueMap:
 
-    regions = {'right_anterior_lobe': RightAnteriorLobe,
-               'left_anterior_lobe': LeftAnteriorLobe,
-               'right_superior_posterior_lobe': RightSuperiorPosteriorLobe,
-               'left_superior_posterior_lobe': LeftSuperiorPosteriorLobe,
-               'right_inferior_posterior_lobe': RightInferiorPosteriorLobe,
-               'left_inferior_posterior_lobe': LeftInferiorPosteriorLobe,
-               'right_lobule_x': RightLobuleX,
-               'left_lobule_x': LeftLobuleX,
-               'vermis': Vermis,
-               'corpus_medullare': CorpusMedullare}
+    regions = dict()
 
     def __init__(self, data, output_filename, show_color=False,
                  font_size=12, stroke='black', stroke_width=2, size=(550, 450)):
@@ -34,3 +42,48 @@ class CerebellumValueMap:
                                          show_color=show_color)
             drawing.add(region.get_svg())
         drawing.save(pretty=True)
+
+
+class CerebellumValueMapLobe(CerebellumValueMap):
+
+    regions = {'right_anterior_lobe': RightAnteriorLobe,
+               'left_anterior_lobe': LeftAnteriorLobe,
+               'right_superioer_posterior_lobe': RightSuperiorPosteriorLobe,
+               'left_superior_posterior_lobe': LeftSuperiorPosteriorLobe,
+               'right_inferior_posterior_lobe': RightInferiorPosteriorLobe,
+               'left_inferior_posterior_lobe': LeftInferiorPosteriorLobe,
+               'right_lobule_x': RightLobuleX,
+               'left_lobule_x': LeftLobuleX,
+               'vermis': Vermis}
+
+
+class CerebellumValueMapLobule(CerebellumValueMap):
+
+    regions = {'Right_Lobules_I-III': RightLobuleI2III,
+               'Left_Lobules_I-III': LeftLobuleI2III,
+               'Right_Lobule_IV': RightLobuleIV,
+               'Left_Lobule_IV': LeftLobuleIV,
+               'Right_Lobule_V': RightLobuleV,
+               'Left_Lobule_V': LeftLobuleV,
+               'Right_Lobule_VI': RightLobuleVI,
+               'Left_Lobule_VI': LeftLobuleVI,
+               'Right_Crus_I': RightLobuleCrusI,
+               'Left_Crus_I': LeftLobuleCrusI,
+               'Right_Crus_II': RightLobuleCrusII,
+               'Left_Crus_II': LeftLobuleCrusII,
+               'Right_Lobule_VIIB': RightLobuleVIIB,
+               'Left_Lobule_VIIB': LeftLobuleVIIB,
+               'Right_Lobule_VIIIA': RightLobuleVIIIA,
+               'Left_Lobule_VIIIA': LeftLobuleVIIIA,
+               'Right_Lobule_VIIIB': RightLobuleVIIIB,
+               'Left_Lobule_VIIIB': LeftLobuleVIIIB,
+               'Right_Lobule_IX': RightLobuleIX,
+               'Left_Lobule_IX': LeftLobuleIX,
+               'Right_Lobule_X': RightLobuleX,
+               'Left_Lobule_X': LeftLobuleX,
+               'Vermis_VI': VermisVI,
+               'Vermis_VII': VermisVII,
+               'Vermis_VIII': VermisVIII,
+               'Vermis_IX': VermisIX,
+               'Vermis_X': VermisX,
+               'Corpus_medullare': CorpusMedullare}
--- scripts/show_cerebellum.py
+++ scripts/show_cerebellum.py
@@ -6,27 +6,45 @@ import argparse
 parser = argparse.ArgumentParser(description='Show cerebellum value map')
 parser.add_argument('-i', '--input-data')
 parser.add_argument('-o', '--output-svg')
+parser.add_argument('-t', '--type', choices={'lobe', 'lobules'}, default='lobe')
 parser.add_argument('-p', '--is-pvalue-mode', default=False,
                     action='store_true')
 
 args = parser.parse_args()
 
-from cerebellum_value_map.cerebellum import CerebellumValueMap
+from cerebellum_value_map.cerebellum import CerebellumValueMapLobe
+from cerebellum_value_map.cerebellum import CerebellumValueMapLobule
 import sys
 import pandas as pd
 import numpy as np
 from cerebellum_value_map.color import ColorConverter
 
 
+# def convert_pval(pval, sign, rev=True):
+#     if pval > 0.05:
+#         result = 0
+#     elif pval > 0.01:
+#         result = 1
+#     elif pval > 0.001:
+#         result = 2
+#     else:
+#         result = 3
+#     result *= np.sign(sign)
+#     if rev:
+#         result *= -1
+#     return result
+
 def convert_pval(pval, sign, rev=True):
-    if pval > 0.05:
+    if pval > 0.01:
         result = 0
-    elif pval > 0.01:
-        result = 1
     elif pval > 0.001:
-        result = 2
+        result = 1
     else:
-        result = 3
+        result = 2
+    # elif pval > 0.001:
+    #     result = 2
+    # else:
+    #     result = 3
     result *= np.sign(sign)
     if rev:
         result *= -1
@@ -39,4 +57,8 @@ if args.is_pvalue_mode:
         data.at[index, 'color'] = convert_pval(data.at[index, 'color'],
                                                data.at[index, 'sign'])
 
-cerebellum = CerebellumValueMap(data, args.output_svg, stroke_width=1)
+sw = 1
+if args.type == 'lobe':
+    CerebellumValueMapLobe(data, args.output_svg, stroke_width=sw)
+else:
+    CerebellumValueMapLobule(data, args.output_svg, stroke_width=sw)
