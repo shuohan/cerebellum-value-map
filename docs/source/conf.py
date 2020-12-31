cee6a2e944004eaa0aac30847bb58c03b6ac2fd1
Shuo Han <shan50@jhu.edu>
2020-12-30 19:21:54 -0500

    Remove pandas from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,5 +61,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ['matplotlib']
+autodoc_mock_imports = ['matplotlib', 'pandas']
 doctest_test_doctest_blocks = ''
